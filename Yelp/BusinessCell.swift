2753d810970f1c64fb35146f8b21de31a3ae245b
Oranuch Tangdechavut <finalehorizon@gmail.com>
2016-01-24 20:19:56 -0800

    - Add Auto layout / Finish the video # 2

--- Yelp/BusinessCell.swift
+++ Yelp/BusinessCell.swift
@@ -37,8 +37,15 @@ class BusinessCell: UITableViewCell {
         // Initialization code
         thumbImageView.layer.cornerRadius = 5
         thumbImageView.clipsToBounds = true
+        
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
     }
 
+    override func layoutSubviews() {
+        super.layoutSubviews()
+         nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+    }
+    
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
